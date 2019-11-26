6eee067a5abdd85afcbfd8a3afade13d6a4f5e42
gui-abq <guilhermeabq.f@gmail.com>
2019-11-26 16:32:53 -0300

    Terminei os components mas não o site

--- semana5/aula2-semana5/future4-Curriculo/curriculof4/src/App.js
+++ semana5/aula2-semana5/future4-Curriculo/curriculof4/src/App.js
@@ -7,16 +7,23 @@ import GuiImg from './imgGui.jpg'
 import DadosUsuario from './dados-componente/dados'
 import EmailIcon from './icones/email-icon.svg'
 import HouseIcon from './icones/house-icon.svg'
+import PageSection from './page-section/PageSection'
+import BotaoCont from './botao-conteudo/BotaoCont'
+import ArrowDown from './icones/down-arrow.svg'
+import FbIcon from './icones/fb-icon.svg'
 
 function App() {
   return (
     <div className="App">
-      <FotoTexto titulo="Gui Albuquerque" imagem={GuiImg} texto="Olá, eu sou o Guilherme. Sou aluno da escola Future4.
-                                                                 Apaixonado por Tecnologia sempre tentando aprender um pouco mais,
-                                                                 meu ponto forte é criatividade. Futuro desenvolvedor Full-Stack"/>
-    
-      <DadosUsuario imagem={EmailIcon} titulo="Email:" texto="guilhermeabq.f@gmail.com"/>
-      <DadosUsuario imagem={HouseIcon} titulo="Endereço:" texto="Av: Jorge Zarur 431"/>
+      <PageSection titulo="Dados Pessoais">
+        <FotoTexto titulo="Gui Albuquerque" imagem={GuiImg} texto="Olá, eu sou o Guilherme. Sou aluno da escola Future4.
+                                                                  Apaixonado por Tecnologia sempre tentando aprender um pouco mais,
+                                                                  meu ponto forte é criatividade. Futuro desenvolvedor Full-Stack"/>
+      
+        <DadosUsuario imagem={EmailIcon} titulo="Email:" texto="guilhermeabq.f@gmail.com"/>
+        <DadosUsuario imagem={HouseIcon} titulo="Endereço:" texto="Av: Jorge Zarur 431"/>
+        <BotaoCont imagem={ArrowDown} texto="Ver mais"/>
+      </PageSection>
     </div>
   );
 }
