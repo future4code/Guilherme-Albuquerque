7b47309a41df8000c7eded365a2a7cceea6f3f57
gui-abq <guilhermeabq.f@gmail.com>
2019-11-28 09:29:33 -0300

    mais posts

--- semana5/aula3-semana5/mediasocial/src/App.js
+++ semana5/aula3-semana5/mediasocial/src/App.js
@@ -10,6 +10,10 @@ import BarraInterativa from './Barra-interativa/BarraInterariva';
 import ContadorLikes from './ContadorLikes/ContadorLikes'
 import LikeProfile from './ContadorLikes/img/profile-foto.jpg'
 import TextoFoto from './TextoFoto/TextoFoto'
+import LogiLogo from './img/logi.png'
+import LogiPost from './img/logi-post.png'
+import NintendoPost from './img/nintendo-post.png'
+import NintendoLogo from './img/nintendo-logo.png'
 
 function App() {
   return (
@@ -17,13 +21,20 @@ function App() {
       <MainPost img={Futureicon} titulo="Future4" verMais={SeeMore} imagemPost={FuturePhoto}/>
 
       <BarraInterativa img={LikeProfile} tituloAutor="Future4" texto="Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                              Phasellus elemen congue in.
-                                              Nunc vulputate enim neque."/>
+                                                                      Phasellus elemen congue in.
+                                                                      Nunc vulputate enim neque."/>
 
-    
+      <MainPost img={LogiLogo} titulo="LogiTech" verMais={SeeMore} imagemPost={LogiPost}/>
 
+      <BarraInterativa img={LikeProfile} tituloAutor="LogiTech" texto="Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                                                      Phasellus elemen congue in.
+                                                                      Nunc vulputate enim neque."/>
 
-    
+      <MainPost img={NintendoLogo} titulo="Nintendo" verMais={SeeMore} imagemPost={NintendoPost}/>
+
+      <BarraInterativa img={LikeProfile} tituloAutor="Nintendo" texto="Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                                                      Phasellus elemen congue in.
+                                                                      Nunc vulputate enim neque."/>                                                                
     </div>
   );
 }
