28cfb43153b491fb72595854c15d10ae5bdba2ff
Guilherme Albuquerque <57281693+gui-abq@users.noreply.github.com>
2020-03-02 13:22:29 -0300

    check rework01

---
+++ backend-semana2/Rework- aula01/build/index.js
@@ -0,0 +1,11 @@
+class Cachorro {
+    latir() {
+        console.log('Au au');
+    }
+    comer(quantidade) {
+        this.peso += quantidade;
+    }
+}
+const astrodog = new Cachorro();
+astrodog.latir();
+//# sourceMappingURL=index.js.map
